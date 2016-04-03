79bc2f2ed53b775e9332284c18a65d3b5ec782a8
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2016-04-03 23:21:12 +0200

    added functionality to generate uart_rx vhdl

--- generatevhdl.py
+++ generatevhdl.py
@@ -1,11 +1,13 @@
 import os
 from myhdl import toVHDL, Signal, ResetSignal, intbv
-from fpgaedu.hdl import BaudGen, UartRx, UartTx, BaudGenRxLookup, Rom
+from fpgaedu.hdl import (BaudGen, UartRx, UartTx, BaudGenRxLookup, Rom)
 
 # Instance constants
-_BAUDRATE = 9600
-_CLK_FREQ = 50000000
-_RX_DIV = 16
+_CLK_FREQ = 100000000
+_UART_BAUDRATE = 9600
+_UART_RX_DIV = 16
+_UART_DATA_BITS = 8
+_UART_STOP_BITS = 1
 
 # toVHDL() constants
 _STD_LOGIC_PORTS = True
@@ -14,11 +16,17 @@ _OUTPUT_DIRECTORY = './vhdl/'
 # Signals
 _CLK = Signal(False)
 _RESET = ResetSignal(True, active=False, async=False)
-_RX_TICK = Signal(False)
-_TX_TICK = Signal(False)
 
-_RX_LOOKUP_ADDR = Signal(intbv(0, min=0, max=_RX_DIV))
-_RX_LOOKUP_DOUT = Signal(intbv(0, min=0, max=16))
+_UART_RX = Signal(False)
+_UART_RX_TICK = Signal(False)
+_UART_RX_DATA = Signal(intbv(0)[_UART_DATA_BITS:0])
+_UART_RX_FINISH = Signal(False)
+_UART_RX_BUSY = Signal(False)
+_UART_RX_BAUD_TICK = Signal(False)
+
+_UART_TX = Signal(False)
+_UART_TX_TICK = Signal(False)
+_UART_TX_DATA = Signal(intbv(0)[_UART_DATA_BITS:0])
 
 def _create_output_directory():
     if not os.path.exists(_OUTPUT_DIRECTORY):
@@ -29,24 +37,27 @@ def _set_tovhdl_defaults(name, directory=_OUTPUT_DIRECTORY):
     toVHDL.name = name
     toVHDL.directory = directory 
 
-def _generate_rom():
-    _set_tovhdl_defaults('rom')
-    content = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
-    toVHDL(Rom, _RX_LOOKUP_DOUT, _RX_LOOKUP_ADDR, content)
-
 def _generate_baudgen_rx_lookup():
     _set_tovhdl_defaults('baudgen_rx_lookup')
     toVHDL(BaudGenRxLookup, _RX_LOOKUP_DOUT, _RX_LOOKUP_ADDR)
 
+def _generate_uart_rx():
+    _set_tovhdl_defaults('uart_rx')
+    toVHDL(UartRx, _CLK, _RESET, _UART_RX, _UART_RX_DATA, _UART_RX_FINISH, 
+            _UART_RX_BUSY, _UART_RX_BAUD_TICK, data_bits=_UART_DATA_BITS, 
+            stop_bits=_UART_STOP_BITS, rx_div=_UART_RX_DIV)
+
 def _generate_baudgen():
     _set_tovhdl_defaults('baudgen')
-    toVHDL(BaudGen, _CLK, _RESET, _RX_TICK, _TX_TICK, clk_freq=_CLK_FREQ,
-            baudrate=_BAUDRATE, rx_div=_RX_DIV)
+    toVHDL(BaudGen, _CLK, _RESET, _UART_RX_TICK, _UART_TX_TICK, 
+            clk_freq=_CLK_FREQ,
+            baudrate=_UART_BAUDRATE, rx_div=_UART_RX_DIV)
 
 if __name__ == '__main__':
     _create_output_directory()
     #_generate_rom()
     #_generate_baudgen_rx_lookup()
+    _generate_uart_rx()
     _generate_baudgen()
 
 
