b35ae444b4e36716ff232a93663ae8760f7a5052
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2016-04-03 23:20:45 +0200

    finished uart rx and test

--- fpgaedu/hdl/_uart_rx.py
+++ fpgaedu/hdl/_uart_rx.py
@@ -1,14 +1,120 @@
-from myhdl import always_comb
+from myhdl import (always_comb, always_seq, Signal, enum, intbv, now)
 
-def UartRx(clk, reset, rx, rx_data, rx_finish, rx_busy, rx_baud_tick):
+def UartRx(clk, reset, rx, rx_data, rx_finish, rx_busy, rx_baud_tick, 
+        data_bits=8, stop_bits=1, rx_div=16):
+    '''
+    clk
+        input
+        Clock Signal
+    reset
+        Input
+        Reset Signal
+    rx
+        Input
+        Uart Rx
+    rx_data
+        Output
+        Reveived data
+    rx_finish
+        Output
+        Pulse signal indicating that rx_data is ready
+    rx_busy
+        Output
+        Indicating that the uart is receiving data
+    rx_baud_tick
+        Input
+        Buadgen input
+    rx_div
+        Parameter
+        Specifying the number of subsamples per tx_tick
+    '''
+
+    state_t = enum('WAIT_START', 'RECV_START', 'RECV_DATA', 'RECV_STOP')
+
+    state_reg = Signal(state_t.WAIT_START)
+    state_next = Signal(state_t.WAIT_START)
+    baud_count_reg = Signal(intbv(0, min=0, max=rx_div))
+    baud_count_next = Signal(intbv(0, min=0, max=rx_div))
+    data_reg = Signal(intbv(0)[data_bits:0])
+    data_next = Signal(intbv(0)[data_bits:0])
+    data_count_reg = Signal(intbv(0, min=0, max=data_bits))
+    data_count_next = Signal(intbv(0, min=0, max=data_bits))
+
+    HIGH = True
+    LOW = False
+    START = LOW
+    STOP = HIGH
+    IDLE = HIGH
+
+    @always_seq(clk.posedge, reset)
+    def register_logic():
+        state_reg.next = state_next
+        baud_count_reg.next = baud_count_next
+        data_count_reg.next = data_count_next
+        data_reg.next = data_next
+        
+    @always_comb
+    def next_state_logic():
+        '''
+        Determine next state levels for registers state_reg, baud_count_reg,
+        data_count_reg and data_reg based on current state and input signals
+        '''
+        state_next.next = state_reg
+        baud_count_next.next = baud_count_reg
+        data_next.next = data_reg
+        data_count_next.next = data_count_reg
+
+        if state_reg == state_t.WAIT_START:
+            data_next.next = 0
+
+            if rx == START and rx_baud_tick == True:
+                state_next.next = state_t.RECV_START
+
+        elif state_reg == state_t.RECV_START:
+            if rx_baud_tick == True:
+                baud_count_next.next = (baud_count_reg + 1) \
+                        % int(rx_div/2)
+
+                if baud_count_reg == int(rx_div/2) - 1:
+                    state_next.next = state_t.RECV_DATA
+
+        elif state_reg == state_t.RECV_DATA:
+            if rx_baud_tick == 1:
+                baud_count_next.next = (baud_count_reg + 1) % rx_div
+
+                if baud_count_reg == rx_div - 1:
+                    data_count_next.next = (data_count_reg + 1) % data_bits
+                    data_next.next[data_count_reg] = rx
+                    
+                    if data_count_reg == data_bits - 1:
+                        state_next.next = state_t.RECV_STOP
+
+        elif state_reg == state_t.RECV_STOP:
+            if rx_baud_tick == 1:
+                baud_count_next.next = (baud_count_reg + 1) % rx_div
+
+                if baud_count_reg == rx_div - 1:
+                    state_next.next = state_t.WAIT_START
 
     @always_comb
     def output_logic():
+        """
+        Determine levels for output signals rx_data, rx_finish, rx_busy based
+        on input signals and current register states 
+        """
+
         rx_finish.next = False
-        rx_busy.next = False
         rx_data.next = 0
 
-        if rx:
-            rx_data.next = 0
+        if state_reg == state_t.WAIT_START:
+            rx_busy.next = False
+        else:
+            rx_busy.next = True
 
-    return output_logic
+        if state_reg == state_t.RECV_STOP and baud_count_reg == rx_div-1 \
+                and rx == STOP:
+            rx_finish.next = True
+            rx_data.next = data_reg
+
+    return register_logic, next_state_logic, output_logic
+
--- tests/test_uart_rx.py
+++ tests/test_uart_rx.py
@@ -1,13 +1,15 @@
 from myhdl import (Signal, ResetSignal, intbv, Simulation, StopSimulation,
-        instance, delay)
+        instance, delay, now)
 from unittest import TestCase
 from fpgaedu.hdl import ClockGen, UartRx
+from random import randint
 
 class UartRxTestCase(TestCase):
 
     DATA_BITS = 8
     STOP_BITS = 1
-    HALF_PERIOD = 10
+    HALF_PERIOD = 1
+    RX_DIV = 2
 
     HIGH = True
     LOW = False
@@ -26,10 +28,12 @@ class UartRxTestCase(TestCase):
 
         self.clockgen = ClockGen(self.clk, self.HALF_PERIOD)
         self.uart_rx = UartRx(self.clk, self.reset, self.rx, self.rx_data, 
-                self.rx_finish, self.rx_busy, self.rx_baud_tick)
-        
+                self.rx_finish, self.rx_busy, self.rx_baud_tick, 
+                data_bits=self.DATA_BITS, stop_bits=self.STOP_BITS,
+                rx_div=self.RX_DIV)
+
     def simulate(self, test_logic, duration=None):
-        sim = Simulation(self.clockgen, self.uart_rx, test_logic)
+        sim = Simulation(self.clockgen, self.uart_rx, *test_logic)
         sim.run(duration, quiet=False)
 
     def stop_simulation(self):
@@ -37,13 +41,47 @@ class UartRxTestCase(TestCase):
 
     def test_rx_receive(self):
 
+        test_data = ['10101010', '00110011', '011001110']
+
+        @instance
+        def monitor():
+            for data in test_data:
+                yield self.rx_finish.posedge
+                print(data)
+                print('bingo! received %s' % bin(self.rx_data))
+                self.assertEquals(self.rx_data, int(data, 2))
+
         @instance
         def test():
-            yield self.clk.negedge
-            self.assertFalse(self.rx_busy)
-            self.assertFalse(self.rx_finish)
+    
+            self.rx.next = self.IDLE
+            self.rx_baud_tick.next = False
+            self.reset.next = False
 
+            yield self.clk.negedge
+            self.reset.next = True
+            yield self.clk.negedge
+
+            def baud_tick(n):
+                for _ in range(n):
+                    for _ in range(49):
+                        yield self.clk.negedge
+                    self.rx_baud_tick.next = True
+                    yield self.clk.negedge
+                    self.rx_baud_tick.next = False
+                    yield self.clk.negedge
+
+            for data in test_data:
+                print('new iteration: now %s' % now())
+                self.rx.next = self.START
+                yield baud_tick(self.RX_DIV)
+                for bit in data[::-1]:
+                    self.rx.next = (bit == '1')
+                    yield baud_tick(self.RX_DIV)
+                self.rx.next = self.STOP
+                yield baud_tick(self.RX_DIV)
+                yield baud_tick(self.RX_DIV)
+                    
             self.stop_simulation()
 
-        self.simulate(test)
-
+        self.simulate([test, monitor])
